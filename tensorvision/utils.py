d3d607d6451cd1b0ff5407161ba68fad72dd6f9c
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-09-24 20:57:28 -0400

    Add more flexibility to utils

--- tensorvision/utils.py
+++ tensorvision/utils.py
@@ -130,23 +130,23 @@ def load_modules_from_hypes(hypes, postfix=""):
 
     # _add_paths_to_sys(hypes)
     f = os.path.join(base_path, hypes['model']['input_file'])
-    data_input = imp.load_source("input%s" % postfix, f)
+    data_input = imp.load_source("input_%s" % postfix, f)
     modules['input'] = data_input
 
     f = os.path.join(base_path, hypes['model']['architecture_file'])
-    arch = imp.load_source("arch%s" % postfix, f)
+    arch = imp.load_source("arch_%s" % postfix, f)
     modules['arch'] = arch
 
     f = os.path.join(base_path, hypes['model']['objective_file'])
-    objective = imp.load_source("objective%s" % postfix, f)
+    objective = imp.load_source("objective_%s" % postfix, f)
     modules['objective'] = objective
 
     f = os.path.join(base_path, hypes['model']['optimizer_file'])
-    solver = imp.load_source("solver%s" % postfix, f)
+    solver = imp.load_source("solver_%s" % postfix, f)
     modules['solver'] = solver
 
     f = os.path.join(base_path, hypes['model']['evaluator_file'])
-    eva = imp.load_source("evaluator%s" % postfix, f)
+    eva = imp.load_source("evaluator_%s" % postfix, f)
     modules['eval'] = eva
 
     return modules
@@ -171,7 +171,7 @@ def _add_paths_to_sys(hypes):
     return
 
 
-def load_modules_from_logdir(logdir):
+def load_modules_from_logdir(logdir, dirname="model_files", postfix=""):
     """Load hypes from the logdir.
 
     Namely the modules loaded are:
@@ -186,19 +186,19 @@ def load_modules_from_logdir(logdir):
     -------
     data_input, arch, objective, solver
     """
-    model_dir = os.path.join(logdir, "model_files")
+    model_dir = os.path.join(logdir, dirname)
     f = os.path.join(model_dir, "data_input.py")
     # TODO: create warning if file f does not exists
-    data_input = imp.load_source("input", f)
+    data_input = imp.load_source("input_%s" % postfix, f)
     f = os.path.join(model_dir, "architecture.py")
-    arch = imp.load_source("arch", f)
+    arch = imp.load_source("arch_%s" % postfix, f)
     f = os.path.join(model_dir, "objective.py")
-    objective = imp.load_source("objective", f)
+    objective = imp.load_source("objective_%s" % postfix, f)
     f = os.path.join(model_dir, "solver.py")
-    solver = imp.load_source("solver", f)
+    solver = imp.load_source("solver_%s" % postfix, f)
 
     f = os.path.join(model_dir, "eval.py")
-    eva = imp.load_source("evaluator", f)
+    eva = imp.load_source("evaluator_%s" % postfix, f)
     modules = {}
     modules['input'] = data_input
     modules['arch'] = arch
@@ -209,7 +209,7 @@ def load_modules_from_logdir(logdir):
     return modules
 
 
-def load_hypes_from_logdir(logdir):
+def load_hypes_from_logdir(logdir, subdir="model_files"):
     """Load hypes from the logdir.
 
     Namely the modules loaded are:
@@ -224,13 +224,16 @@ def load_hypes_from_logdir(logdir):
     -------
     hypes
     """
-    hypes_fname = os.path.join(logdir, "model_files/hypes.json")
+    model_dir = os.path.join(logdir, subdir)
+    hypes_fname = os.path.join(model_dir, "hypes.json")
     with open(hypes_fname, 'r') as f:
         logging.info("f: %s", f)
         hypes = json.load(f)
     _add_paths_to_sys(hypes)
     hypes['dirs']['base_path'] = os.path.realpath(logdir)
     hypes['dirs']['output_dir'] = os.path.realpath(logdir)
+    hypes['dirs']['image_dir'] = os.path.join(hypes['dirs']['output_dir'],
+                                              'images')
 
     return hypes
 
@@ -381,6 +384,39 @@ def overlay_segmentation(input_image, segmentation, color_dict):
                 output.putpixel((y, x), color_dict[segmentation[x, y]])
             elif 'default' in color_dict:
                 output.putpixel((y, x), color_dict['default'])
+
+    background = scipy.misc.toimage(input_image)
+    background.paste(output, box=None, mask=output)
+
+    return np.array(background)
+
+
+def fast_overlay(input_image, segmentation, color=[0, 255, 0, 127]):
+    """
+    Overlay input_image with a hard segmentation result for two classes.
+
+    Store the result with the same name as segmentation_image, but with
+    `-overlay`.
+
+    Parameters
+    ----------
+    input_image : numpy.array
+        An image of shape [width, height, 3].
+    segmentation : numpy.array
+        Segmentation of shape [width, height].
+    color: color for forground class
+
+    Returns
+    -------
+    numpy.array
+        The image overlayed with the segmenation
+    """
+    color = np.array(color).reshape(1, 4)
+    shape = input_image.shape
+    segmentation = segmentation.reshape(shape[0], shape[1], 1)
+
+    output = np.dot(segmentation, color)
+    output = scipy.misc.toimage(output, mode="RGBA")
 
     background = scipy.misc.toimage(input_image)
     background.paste(output, box=None, mask=output)
